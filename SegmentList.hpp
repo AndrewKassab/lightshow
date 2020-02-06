6043abe912d07f7374567039698a424627a679e5
andrewkassab <andrewkassab@live.com>
2020-02-05 17:17:23 -0800

    added more documentation and make list variables private

--- SegmentList.hpp
+++ SegmentList.hpp
@@ -5,28 +5,42 @@
 #include "LightSegment.hpp"
 
 class SegmentList{
-  public:
+  private:
+    int size; // number of segments in list
     SegmentNode * head;
-    int size;
+  public:
     SegmentList(LightSegment * head);
     ~SegmentList();
     void add(LightSegment * newSegment);
+
+    // sets all segments in list to color
     void setAllToColor(CRGB color);
+
     void turnAllOff();
+    
+    // returns length of the longest lightsegment in the list
     int getLongestSegmentSize();
 
-    // lights fade in towards their rgb values 
+    // lights fade up towards their rgb values 
     void fadeAllIn(int delayTime, CRGB color);
 
-    // lights fade down from their rgb until they're off 
+    // lights fade down from their rgb value until they're off 
     void fadeAllDown(int delayTime);
 
-    // lights perform a trace, reverse arrayindicates if a specific segment should trace in reverse
+    /* lights perform a trace and remain on as they trace, reverse array indicates 
+     * if a specific segment at that index should trace in reverse
+     */
     void traceAllAndRemain(int thickness, int delayTime, boolean * reverse, CRGB color);
 
-    // lights perform a trace, reverse arrayindicates if a specific segment should trace in reverse
+    /* lights perform a trace and turn off as they trace, reverse array indicates 
+     * if a specific segment at that index should trace in reverse
+     */
     void traceAllDontRemain(int thickness, int delayTime, boolean * reverse, CRGB color);
 
+    /* lights perform a trace and turn off as they trace, but the last few stay
+     * on, reverse array indicates if a specific segment at that index should 
+     * trace in reverse
+     */
     void traceAllKeepEnd(int thickness, int delayTime, boolean * reverse, CRGB color);
 };
 
